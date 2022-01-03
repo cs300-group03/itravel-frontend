cd693193e8f83625ae6d2899a9d89e704e24076d
LeThaoHuyen <lethaohuyen3010@gmail.com>
2022-01-03 22:15:44 +0700

    add theme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import './App.css';
 import Header from './components/Header/Header';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/styles';
+import { createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  spacing: 4,
+  palette: {
+    primary: {
+      main: '#F4A442'
+    },
+    secondary: {
+      main: '#125C13'
+    }
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <Header user={true} />
-      <Routes>
-        <Route path='/' element=""/>
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+        <Router>
+          <Header user={false} />
+          <Routes>
+            <Route path='/' element=""/>
+          </Routes>
+        </Router>
+    </ThemeProvider>
+    
   );
 }
 

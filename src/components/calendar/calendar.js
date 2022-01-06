6d85ad44d0b8645888d432da5eb0dd42cd2428b1
Vân Lâm <63760071+van2510@users.noreply.github.com>
2022-01-06 20:23:03 +0700

    add feature to homepage

---
+++ src/components/calendar/calendar.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { render } from "react-dom";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';    
+
+const ReactCalendar = () => {
+
+    const [date, setDate] = useState(new Date());
+
+    const onChange = date => {
+      setDate(date);
+    };
+
+  return (
+    <div>
+      <Calendar onChange={onChange} value={date} />
+      {console.log(date)}
+      {date.toString()}
+    </div>
+  );
+};
+render(<ReactCalendar/>,document.querySelector("#root"))
+export default ReactCalendar;

554654dc2959caa0dfb820589f9729ef8d646ebf
LeThaoHuyen <lethaohuyen3010@gmail.com>
2022-01-07 20:11:57 +0700

    add publishSchedulePage

--- src/pages/publish-schedule-page/index.js
+++ src/pages/publish-schedule-page/index.js
@@ -5,46 +5,33 @@ import Box from '@mui/material/Box'
 import Drawer from '@mui/material/Drawer'
 import CssBaseline from '@mui/material/CssBaseline'
 import Toolbar from '@mui/material/Toolbar'
-import List from '@mui/material/List'
 import Typography from '@mui/material/Typography'
-import Divider from '@mui/material/Divider'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
 import ChevronRightIcon from '@mui/icons-material/ChevronRight'
-import ListItem from '@mui/material/ListItem'
-import ListItemText from '@mui/material/ListItemText'
 import ScheduleCalendar from '../../components/schedule-calendar'
 import { Link } from 'react-router-dom'
-import { Button } from '@mui/material'
-import SearchAttraction from '../../components/searchbox/search-attraction'
-import SearchService from '../../components/searchbox/search-service'
+import { Button, Card } from '@mui/material'
 import { Main, AppBar, DrawerHeader } from '../schedule-page/elements'
-import PublishAlertDialog from '../../components/dialog/publish-dialog'
-import SearchResultCard from '../../components/searchResultCard'
+import DateRangeIcon from '@mui/icons-material/DateRange'
+import LocationOnIcon from '@mui/icons-material/LocationOn'
+import CardContent from '@mui/material/CardContent'
 import { useSelector } from 'react-redux'
+import { ThumbDown, ThumbUp } from '@mui/icons-material'
 
 const drawerWidth = 340
 
 export default function PublishSchedulePage() {
-  const schedule = useSelector(state => state.schedule.currentSchedule);
+  const schedule = useSelector((state) => state.schedule.currentSchedule)
   const theme = useTheme()
   const [open, setOpen] = React.useState(true)
-  const [openPublishDialog, setOpenPublishDialog] = React.useState(false)
   const handleDrawerOpen = () => {
     setOpen(true)
   }
 
   const handleDrawerClose = () => {
     setOpen(false)
-  }
-
-  const handleOpenDialog = () => {
-    setOpenPublishDialog(true)
-  }
-
-  const handleDialogClose = () => {
-    setOpenPublishDialog(false)
   }
 
   return (
@@ -64,16 +51,14 @@ export default function PublishSchedulePage() {
           <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
             {/* {schedule.title} */} My Trip To DaLat
           </Typography>
-          <Button onClick={handleOpenDialog} color="inherit">
-            Save
-          </Button>
-          <PublishAlertDialog
-            open={openPublishDialog}
-            handleClose={handleDialogClose}
-          />
-          <Button color="inherit" sx={{ mr: 2 }}>
-            Duplicate
-          </Button>
+          <IconButton color="neutral" aria-label="upvote" component="span">
+            <ThumbUp />
+            <Typography sx={{ ml: 2, fontSize: 20 }}>1453</Typography>
+          </IconButton>
+          <IconButton color="neutral" aria-label="upvote" component="span">
+            <ThumbDown />
+            <Typography sx={{ ml: 2, fontSize: 20, mr: 2 }}>2</Typography>
+          </IconButton>
           <Link to="\">
             <Avatar src="https://icdn.dantri.com.vn/2017/emma-watson-5-1488809769584.jpg" />
           </Link>
@@ -95,7 +80,8 @@ export default function PublishSchedulePage() {
         <DrawerHeader>
           <Box
             component="img"
-            src={schedule.img}
+            // src={schedule.img}
+            src="https://media.tacdn.com/media/attractions-splice-spp-674x446/07/12/61/e5.jpg"
             sx={{ height: 150, position: 'relative', width: '100%' }}
           ></Box>
           <IconButton
@@ -115,49 +101,89 @@ export default function PublishSchedulePage() {
             )}
           </IconButton>
         </DrawerHeader>
-        <Box sx={{ padding: 3 }}>
-          <SearchAttraction />
-          <List spacing="2">
-            <ListItem>
-              <SearchResultCard
-                title={'Valley of Love'}
-                address={'357 Mai Anh Dao, Da Lat, Lam Dong'}
-                img={
-                  'https://www.vietnamonline.com/media/uploads/froala_editor/images/VNO-tlty1.jpg'
-                }
-              />
-            </ListItem>
-            <ListItem>
-              <SearchResultCard
-                title={'Hon Mun'}
-                address={'Hon Mun, Vinh Nguyen, Nha Trang City, Khanh Hoa'}
-                img={
-                  'https://vinhnhatrang.net/wp-content/uploads/2019/10/hon-mun-1.jpg'
-                }
-              />
-            </ListItem>
-          </List>
-          <SearchService />
-          <List>
-            <ListItem>
-              <SearchResultCard
-                title={'Sandals Restaurant'}
-                address={'72-74 Tran Phu, Nha Trang'}
-                img={
-                  'https://localinsider.storage.googleapis.com/size/w1000/2021/10/DSC_2460-scaled.jpg'
-                }
-              />
-            </ListItem>
-            <ListItem>
-              <SearchResultCard
-                title={'Vinpearl Luxury'}
-                address={'73 Hung Vuong, Nha Trang'}
-                img={
-                  'https://media.expedia.com/hotels/5000000/4430000/4424400/4424377/3e681f92_z.jpg'
-                }
-              />
-            </ListItem>
-          </List>
+        <Card
+          sx={{
+            padding: 3,
+            mt: -10,
+            zIndex: 2,
+            width: '90%',
+            ml: 'auto',
+            mr: 'auto',
+          }}
+        >
+          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Typography
+              component="div"
+              sx={{ fontSize: 20, textAlign: 'center', fontWeight: '600' }}
+            >
+              Trip to Nha Trang Yay!!
+            </Typography>
+
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                mt: 2,
+              }}
+            >
+              <DateRangeIcon></DateRangeIcon>
+              <Typography sx={{ fontSize: 11, ml: 1 }}>
+                22 Dec, 2020 - 28 Dec, 2020
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                mt: 1,
+              }}
+            >
+              <LocationOnIcon></LocationOnIcon>
+              <Typography sx={{ fontSize: 11, ml: 1 }}> Dalat </Typography>
+            </Box>
+          </Box>
+        </Card>
+        <Box
+          sx={{ padding: 4, display: 'flex', flexDirection: 'column', mt: 2 }}
+        >
+          <Box
+            sx={{display: 'flex', flexDirection: 'row', alignItems: 'center', mt: 2}}
+          >
+            <Avatar src="https://kenh14cdn.com/thumb_w/660/2019/4/24/e-1556084674630222748642.jpg" />
+            <Typography sx={{ fontSize: 12, ml: 1, fontWeight: '500' }}>
+              Shawn Mendes
+            </Typography>
+          </Box>
+          <Typography
+            variant="subtitle1"
+            color="text.secondary"
+            component="div"
+            sx={{ fontSize: 10, padding: 2 }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </Typography>
+          <Button
+            sx={{ mt: 2, mr: 'auto', ml: 'auto', width: '70%' }}
+            color="primary"
+            variant="contained"
+          >
+            Save schedule
+          </Button>
+          <Button
+            sx={{ mt: 2, mr: 'auto', ml: 'auto', width: '70%' }}
+            color="secondary"
+            variant="contained"
+          >
+            Duplicate schedule
+          </Button>
         </Box>
       </Drawer>
       <Main open={open}>
